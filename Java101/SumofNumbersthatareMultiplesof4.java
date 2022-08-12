854c75906181bed881ae27885c87cdbf08c7b8f1
Burak Dilber <66571118+burakdilber@users.noreply.github.com>
2022-08-12 23:12:03 +0300

    Create SumofNumbersthatareMultiplesof4.java

---
+++ Java101/SumofNumbersthatareMultiplesof4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int sayi, toplam = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Sayı Giriniz : ");
+            sayi = input.nextInt();
+            if (sayi % 4 == 0) {
+                toplam += sayi;
+            }
+        } while(sayi % 2 == 0);
+
+        System.out.println("Toplam : " + toplam);
+    }
+}
